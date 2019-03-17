4b0a5cc5887e1ae654ff9d2b137b0ef4e0f05e7a
xiangzhenbiao <xiangzhenbiao@fangdd.com>
2019-03-17 17:15:09 +0800

    组件根据动态配置进行集成调试或者独立运行

---
+++ share/src/main/java/com/xzb/share/ShareMainActivity.java
@@ -0,0 +1,13 @@
+package com.xzb.share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_main);
+    }
+}
